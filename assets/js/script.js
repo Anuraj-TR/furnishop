71c317a83e3bc3d680ccd72c3171d2977e9deba6
Anuraj <101694416+Anuraj-TR@users.noreply.github.com>
2022-06-15 13:33:57 +0530

    testimonial completed
    added buttons

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+$(".fs-testimonials__carousel").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  //   autoplay: true,
+  //   autoplaySpeed: 2000,
+  arrows: true,
+  infinite: true,
+  
+});
+
+
